268087b2fb575ceeff29bb1b4b80884e16f3bf58
Ygao-Underground <ygaounderground011@gmail.com>
2025-04-08 10:47:16 -0300

    feat: Cria as migrations

---
+++ database/migrations/2025_03_27_214058_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('car_user_id')->constrained('car_user');
+            $table->foreignId('repair_shop_id')->constrained('repair_shops');
+            $table->foreignId('status')->constrained('statuses');
+            $table->text('description');
+            $table->integer('total_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
